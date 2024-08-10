71e2d01d687f4b61258996ab497d647ebbb763ec
lazydino <woohs0130@naver.com>
2024-08-10 19:48:34 +0900

    Fix-esc key acting as cr in commandmod problem solved

--- lua/backtoeng/init.lua
+++ lua/backtoeng/init.lua
@@ -4,14 +4,19 @@ function M.setup(opts)
 	function M.InputToEng()
 		os.execute(opts.toEng)
 	end
-	vim.keymap.set({ "n", "c", "v", "x", "i" }, "<Esc>", function()
+	vim.keymap.set({ "n", "v", "x", "i" }, "<Esc>", function()
 		M.InputToEng()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
+	end, { noremap = true, silent = true })
+
+	vim.keymap.set("c", "<Esc>", function()
+		M.InputToEng()
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", false)
 	end, { noremap = true, silent = true })
 
 	vim.keymap.set({ "c", "v", "x" }, "<CR>", function()
 		M.InputToEng()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true), "n", true)
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true), "n", false)
 	end, { noremap = true, silent = true })
 end
 
