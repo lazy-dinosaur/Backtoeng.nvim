400ec49d67778313e486205d3e3bf6617b5331fe
lazydino <woohs0130@naver.com>
2024-08-12 09:02:50 +0900

    AutcmdTest

--- lua/backtoeng/init.lua
+++ lua/backtoeng/init.lua
@@ -4,20 +4,31 @@ function M.setup(opts)
 	function M.InputToEng()
 		os.execute(opts.toEng)
 	end
-	vim.keymap.set({ "n", "v", "x", "i" }, "<Esc>", function()
-		M.InputToEng()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
-	end, { noremap = true, silent = true })
+	function M.setKeys()
+		print("keyset!")
+		vim.keymap.set({ "n", "v", "x", "i" }, "<Esc>", function()
+			M.InputToEng()
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
+		end, { noremap = true, silent = true })
 
-	vim.keymap.set("c", "<Esc>", function()
-		M.InputToEng()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", false)
-	end, { noremap = true, silent = true })
+		vim.keymap.set("c", "<Esc>", function()
+			M.InputToEng()
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", false)
+		end, { noremap = true, silent = true })
 
-	vim.keymap.set({ "c", "v", "x" }, "<CR>", function()
-		M.InputToEng()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true), "n", false)
-	end, { noremap = true, silent = true })
+		vim.keymap.set({ "c", "v", "x" }, "<CR>", function()
+			M.InputToEng()
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true), "n", false)
+		end, { noremap = true, silent = true })
+	end
 end
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+	-- group = BackToEngSetup,
+	callback = function()
+		print("loaded!")
+		require("backtoeng").setKeys()
+	end,
+})
 
 return M
