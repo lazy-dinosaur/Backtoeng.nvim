e224dc43a11418aca137921743711a2ddd7dd3f8
lazydino <woohs0130@naver.com>
2024-08-12 13:34:54 +0900

    Fix: fuzzyfinder 사용시 하이라이트 사라지지 않는 문제 해결

--- lua/backtoeng/init.lua
+++ lua/backtoeng/init.lua
@@ -7,7 +7,7 @@ function M.setup(opts)
 	function M.setKeys()
 		vim.keymap.set({ "n", "v", "x", "i" }, "<Esc>", function()
 			M.InputToEng()
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>noh<CR><Esc>", true, true, true), "n", false)
 		end, { noremap = true, silent = true })
 
 		vim.keymap.set("c", "<Esc>", function()
